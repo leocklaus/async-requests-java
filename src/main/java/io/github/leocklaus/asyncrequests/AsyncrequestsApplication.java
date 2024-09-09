885ec713302623c14efa0d06e8a584cb23131484
leocklaus <leocklaus@gmail.com>
2024-09-09 17:15:49 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/asyncrequests/AsyncrequestsApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.asyncrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncrequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncrequestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/asyncrequests/AsyncrequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.asyncrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncrequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
